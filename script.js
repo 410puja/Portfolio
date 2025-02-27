94de589126747d21f5297266080664a1cc31237e
410puja <108603018+410puja@users.noreply.github.com>
2025-02-27 21:41:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 },
+    },
   });
 });
